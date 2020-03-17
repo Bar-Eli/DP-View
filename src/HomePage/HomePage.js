92c12e8c7adcac8c348a713bc52d5cc195152d54
nadavatar <nadavatar64@gmail.com>
2020-03-17 10:52:54 +0200

    test

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,32 +1,29 @@
-import React from 'react';
-import './HomePage.css';
-import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-
-
+import React from "react";
+import "./HomePage.css";
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 
 class HomePage extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            toastState: false,
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      toastState: false
     };
+  }
 
+  showAlert = () => {
+    alert("Trying to show the alert on screen");
+  };
 
-
-    render() {
-
-        return (
-
-            <div>
-                <Button variant="contained" color="primary">Primary</Button>
-            </div>
-
-        );
-    }
-
+  render() {
+    return (
+      <div>
+        <Button variant="contained" color="primary" onClick={this.showAlert}>
+          Primary
+        </Button>
+      </div>
+    );
+  }
 }
 
 export default HomePage;

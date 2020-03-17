29925ae6f1a7a39fb5239bd2125edde860c12555
nadavatar <nadavatar64@gmail.com>
2020-03-17 16:21:27 +0200

    React Router Works

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,32 +3,81 @@ import React, { Component } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 import styled from "styled-components";
+import Link from "@material-ui/core/Link";
+import { withRouter } from "react-router-dom";
 
-export default class Navbar extends Component {
+export class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isRedirect: false
+    };
+  }
+
   NavbarStyle = styled.div`
     #text {
       font-weight: bold;
       font-size: 350%;
+      margin-right: 50px;
+    }
+    .btn {
+      color: white;
+      margin-right: 100px;
+      font-weight: bold;
+      font-size: 100%;
     }
   `;
+
+  handleRedirect = num => {
+    if (num === 1) {
+      this.props.history.push("/");
+    }
+    if (num === 2) {
+      this.props.history.push("/addRule");
+    }
+    if (num === 3) {
+      this.props.history.push("/editRule");
+    }
+  };
 
   render() {
     return (
       <this.NavbarStyle>
         <AppBar position="static">
           <Toolbar variant="dense">
-            {/* <IconButton edge="start" color="inherit" aria-label="menu">
-              <MenuIcon />
-            </IconButton> */}
             <Typography color="inherit" id="text">
               DP-View
             </Typography>
+            <Link
+              component="button"
+              variant="body2"
+              onClick={() => this.handleRedirect(1)}
+              className="btn"
+            >
+              Add New MPGW
+            </Link>
+            <Link
+              component="button"
+              variant="body2"
+              onClick={() => this.handleRedirect(2)}
+              className="btn"
+            >
+              Add New Rule
+            </Link>
+            <Link
+              component="button"
+              variant="body2"
+              onClick={() => this.handleRedirect(3)}
+              className="btn"
+            >
+              Edit Existing Rule
+            </Link>
           </Toolbar>
         </AppBar>
       </this.NavbarStyle>
     );
   }
 }
+
+export default withRouter(Navbar);

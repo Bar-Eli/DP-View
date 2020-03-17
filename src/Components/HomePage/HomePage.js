eef44719ec8551a8cee396b93c1fd62c5209c4ee
nadavatar <nadavatar64@gmail.com>
2020-03-17 13:22:14 +0200

    Added navbar

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,22 +1,26 @@
-import React from "react";
-import "./HomePage.css";
-import Button from "@material-ui/core/Button";
+import React, { Component } from "react";
+// import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 
-class HomePage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      toastState: false
-    };
-  }
-
+export default class HomePage extends Component {
   render() {
     return (
       <div>
-        <h1>DP-View</h1>
+        <AppBar position="static">
+          <Toolbar variant="dense">
+            {/* <IconButton edge="start" color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton> */}
+            <Typography variant="h4" color="inherit">
+              <b>DP-View</b>
+            </Typography>
+          </Toolbar>
+        </AppBar>
       </div>
     );
   }
 }
-
-export default HomePage;

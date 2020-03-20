7bad940e77eab562ae74c62bd7d3bde160e8a6a3
nadavatar <nadavatar64@gmail.com>
2020-03-20 15:24:59 +0200

    Added DynamicTable Component for future uses

---
+++ src/Components/DynamicTable.js
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import MaterialTable from "material-table";
+
+export class DynamicTable extends Component {
+  constructor(props) {
+    super(props);
+    // Don't call this.setState() here!
+    this.state = {
+      columns: [
+        { title: "Name", field: "name" },
+        { title: "Surname", field: "surname" },
+        { title: "Birth Year", field: "birthYear", type: "numeric" },
+        {
+          title: "Birth Place",
+          field: "birthCity",
+          lookup: { 34: "İstanbul", 63: "Şanlıurfa" }
+        }
+      ],
+      data: [
+        { name: "Mehmet", surname: "Baran", birthYear: 1987, birthCity: 63 },
+        {
+          name: "Zerya Betül",
+          surname: "Baran",
+          birthYear: 2017,
+          birthCity: 34
+        }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <MaterialTable
+          title="Editable Example"
+          columns={this.props.columns}
+          data={this.props.data}
+          editable={{
+            onRowAdd: newData =>
+              new Promise(resolve => {
+                setTimeout(() => {
+                  resolve();
+                  this.setState(prevState => {
+                    const data = [...prevState.data];
+                    data.push(newData);
+                    return { ...prevState, data };
+                  });
+                }, 600);
+              }),
+            onRowUpdate: (newData, oldData) =>
+              new Promise(resolve => {
+                setTimeout(() => {
+                  resolve();
+                  if (oldData) {
+                    this.setState(prevState => {
+                      const data = [...prevState.data];
+                      data[data.indexOf(oldData)] = newData;
+                      return { ...prevState, data };
+                    });
+                  }
+                }, 600);
+              }),
+            onRowDelete: oldData =>
+              new Promise(resolve => {
+                setTimeout(() => {
+                  resolve();
+                  this.setState(prevState => {
+                    const data = [...prevState.data];
+                    data.splice(data.indexOf(oldData), 1);
+                    return { ...prevState, data };
+                  });
+                }, 600);
+              })
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+export default DynamicTable;

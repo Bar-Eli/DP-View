aef5804bf5a315b5cd616dd554462443a6d15ab8
nadavatar <nadavatar1@walla.com>
2020-03-26 11:52:16 +0200

    commit

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -2,11 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NewRoutePage from "./Components/NewMPGWForm/NewRoutePage";
 import EditRuleStepper from "./Components/EditRuleForm/EditRules";
-<<<<<<< HEAD
 import RuleTable from "./Components/RuleTable";
-=======
 import HorizontalPage from "./Components/NewMPGWHorizontal/HorizontalPage";
->>>>>>> horizontalStepper
 // import HorizontalNonLinearAlternativeLabelStepper from "./Components/HorizontalStepper";
 
 function AppRouter() {
@@ -16,69 +13,69 @@ function AppRouter() {
         <Route path="/" exact component={NewRoutePage} />
         {/* <Route path="/addRule" component={NewRulePage} /> */}
         <Route path="/editRule" component={EditRuleStepper} />
-<<<<<<< HEAD
-        <Route path="/rule" component={() => <RuleTable data={{
-          "details": {
-            "projectNameValue": "",
-            "projectMadorValue": "",
-            "projectTeamValue": "",
-            "testOrProd": ""
-          },
-          "rules": [
-            {
-              "name": "Logs_To_Elastic",
-              "srcAddr": {
-                "network": "test",
-                "protocol": "HTTP",
-                "primaryAddress": "124.25.2.2",
-                "secondaryAddress": "5885",
-                "methods": [
-                  "get",
-                  "post"
+        <Route
+          path="/rule"
+          component={() => (
+            <RuleTable
+              data={{
+                details: {
+                  projectNameValue: "",
+                  projectMadorValue: "",
+                  projectTeamValue: "",
+                  testOrProd: ""
+                },
+                rules: [
+                  {
+                    name: "Logs_To_Elastic",
+                    srcAddr: {
+                      network: "test",
+                      protocol: "HTTP",
+                      primaryAddress: "124.25.2.2",
+                      secondaryAddress: "5885",
+                      methods: ["get", "post"]
+                    },
+                    destAddr: {
+                      network: "test2",
+                      protocol: "MQ",
+                      primaryAddress: "TEST_QM",
+                      secondaryAddress: "TEST_QUEUE",
+                      methods: []
+                    },
+                    filter: {
+                      filterType: "dpas",
+                      dpasFilter: "dexter",
+                      schemaPath: ""
+                    }
+                  },
+                  {
+                    name: "Data_To_Shit",
+                    srcAddr: {
+                      network: "test",
+                      protocol: "HTTP",
+                      primaryAddress: "124.25.2.2",
+                      secondaryAddress: "5885",
+                      methods: ["post"]
+                    },
+                    destAddr: {
+                      network: "test2",
+                      protocol: "MQ",
+                      primaryAddress: "TEST_QM",
+                      secondaryAddress: "TEST_QUEUE",
+                      methods: []
+                    },
+                    filter: {
+                      filterType: "dpas",
+                      dpasFilter: "dexter",
+                      schemaPath: ""
+                    }
+                  }
                 ]
-              },
-              "destAddr": {
-                "network": "test2",
-                "protocol": "MQ",
-                "primaryAddress": "TEST_QM",
-                "secondaryAddress": "TEST_QUEUE",
-                "methods": []
-              },
-              "filter": {
-                "filterType": "dpas",
-                "dpasFilter": "dexter",
-                "schemaPath": ""
-              }
-            },
-            {
-              "name": "Data_To_Shit",
-              "srcAddr": {
-                "network": "test",
-                "protocol": "HTTP",
-                "primaryAddress": "124.25.2.2",
-                "secondaryAddress": "5885",
-                "methods": [
-                  "post"
-                ]
-              },
-              "destAddr": {
-                "network": "test2",
-                "protocol": "MQ",
-                "primaryAddress": "TEST_QM",
-                "secondaryAddress": "TEST_QUEUE",
-                "methods": []
-              },
-              "filter": {
-                "filterType": "dpas",
-                "dpasFilter": "dexter",
-                "schemaPath": ""
-              }
-            }
-          ]
-        }} title="Overview" />} />
-=======
+              }}
+              title="Overview"
+            />
+          )}
+        />
         <Route path="/horizontal" component={HorizontalPage} />
->>>>>>> horizontalStepper
       </div>
     </Router>
   );

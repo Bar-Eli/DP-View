b143910b86f8c2f854e1061d6997e50d6c99eafa
nadavatar <nadavatar1@walla.com>
2020-03-28 14:38:25 +0300

    before pulling origin master

--- src/Components/NewMPGWForm/RouteStepper/HorizontalStepper.js
+++ src/Components/NewMPGWForm/RouteStepper/HorizontalStepper.js
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Stepper from "@material-ui/core/Stepper";
 import Step from "@material-ui/core/Step";
 import StepButton from "@material-ui/core/StepButton";
+import Alert from "@material-ui/lab/Alert";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import FilterFormForm from "../../NewMPGWForm/RouteStepper/FilterForm";
@@ -203,7 +204,7 @@ export default function HorizontalNonLinearAlternativeLabelStepper({
       }
     });
     onRulesChange(newRules);
-    rules = newRules;
+    alert("Your rules have been submitted!");
   };
 
   const handleReset = () => {

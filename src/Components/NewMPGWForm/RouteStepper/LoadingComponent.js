0d61df026723d5875224e68e228ada3d0226abae
Hadar Nir <hadar4nir@gmail.com>
2020-03-29 07:53:10 +0000

    added loading component

---
+++ src/Components/NewMPGWForm/RouteStepper/LoadingComponent.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+import clsx from 'clsx';
+import { createStyles, makeStyles, Theme, withStyles  } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { green } from '@material-ui/core/colors';
+import Button from '@material-ui/core/Button';
+import Fab from '@material-ui/core/Fab';
+import CheckIcon from '@material-ui/icons/Check';
+import SaveIcon from '@material-ui/icons/Save';
+import { render } from '@testing-library/react';
+import CreateIcon from '@material-ui/icons/Create';
+
+const useStyles = theme => ({
+    root: {
+        display: 'flex',
+        alignItems: 'center',
+      },
+      wrapper: {
+        margin: theme.spacing(1),
+        position: 'relative',
+      },
+      buttonSuccess: {
+        backgroundColor: green[500],
+        '&:hover': {
+          backgroundColor: green[700],
+        },
+      },
+      fabProgress: {
+        color: green[500],
+        position: 'absolute',
+        top: -6,
+        left: -6,
+        zIndex: 1,
+      },
+      buttonProgress: {
+        color: green[500],
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        marginTop: -12,
+        marginLeft: -12,
+      }
+  });
+
+class CircularIntegration extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+          loading: false,
+          success: false,
+          buttonClass: "",
+
+        };
+        
+      }
+
+  handleButtonClick = () => {
+    const { classes } = this.props;
+    this.setState({ loading: true});
+
+    const timer = setTimeout(() => {
+        this.setState({ 
+            loading: false,
+            success: true,
+            buttonClass: classes.buttonSuccess
+        });
+      }, 1000);
+    return () => clearTimeout(timer);
+  }
+
+
+  render(){
+    const { classes } = this.props;
+  return (
+    <div className={classes.root}>
+      <div className={classes.wrapper}>
+        <Button
+          variant="contained"
+          color="primary"
+          className={this.state.buttonClass}
+          disabled={this.state.success}
+          onClick={this.handleButtonClick}
+        >
+         Create
+        </Button>
+        {this.state.loading && <CircularProgress size={24} className={classes.buttonProgress} />}
+      </div>
+      <div className={classes.wrapper}>
+        <Fab
+          aria-label="create route"
+          color="primary"
+          className={this.state.buttonClass}
+        >
+          {this.state.success ? <CheckIcon /> : <CreateIcon fontSize='large' />}
+        </Fab>
+        {this.state.loading && <CircularProgress size={68} className={classes.fabProgress} />}
+      </div>
+      <div className={classes.wrapper}>
+        <Fab
+          aria-label="create route"
+          color="primary"
+          className={this.state.buttonClass}
+        >
+          {this.state.success ? <CheckIcon /> : <CreateIcon fontSize='large' />}
+        </Fab>
+        {this.state.loading && <CircularProgress size={68} className={classes.fabProgress} />}
+      </div>
+    </div>
+  );
+  }
+}
+
+export default withStyles(useStyles, { withTheme: true })(CircularIntegration);

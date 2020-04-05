92e1aa7542138ce286eeac5410c8b7b26a70af62
Hadar Nir <hadarnir33@gmail.com>
2020-04-05 16:02:15 +0300

    ready for backed request

--- src/Components/EditRulesForm/RouteStepper/DetailsForm.js
+++ src/Components/EditRulesForm/RouteStepper/DetailsForm.js
@@ -23,7 +23,7 @@ class DetailsForm extends Component {
     this.state = {
       testBtnColor: "primary",
       prodBtnColor: "default",
-      mpgwList: ["Incognito", "Outcognito", "IceCube", "Spotify"],
+      mpgwList: [],
       environmentsList: ["ZadokCluster", "Salim", "Zeus"],
       // verify if form is complete somehow
     };
@@ -43,6 +43,13 @@ class DetailsForm extends Component {
     this.checkIfAllValid();
   }
 
+  setMpgwArray = (clusterName, testOrProd) => {
+    // Wait for a resopnse from the backend for mpgw names for the given cluster and environment
+    // let mpgwList = await this.props.getArray(clusterName, testOrProd);
+    let mpgwList = ["Incognito", "Outcognito", "IceCube", "Spotify"];
+    this.setState({ mpgwList: mpgwList })
+  }
+
   checkIfAllValid = () => {
     //Check if the validators were initialized, if so update valid props to true
     if (this.validator.allValid()) {
@@ -50,12 +57,19 @@ class DetailsForm extends Component {
     } else this.props.validationHandler(false);
   };
 
-  testBtnClick = () => {
-    this.setState({ testBtnColor: "primary", prodBtnColor: "default" });
+  testBtnClick = () => {this.setState({ });
+    this.setState({ 
+      testBtnColor: "primary", 
+      prodBtnColor: "default",
+      mpgwList: []
+    });
   };
 
   prodBtnClick = () => {
-    this.setState({ prodBtnColor: "primary", testBtnColor: "default" });
+    this.setState({ prodBtnColor: "primary", 
+    testBtnColor: "default",
+    mpgwList: []
+   });
   };
 
   render() {
@@ -69,7 +83,7 @@ class DetailsForm extends Component {
             id="cluster"
             options={this.state.environmentsList}
             getOptionLabel={environmentsList => environmentsList}
-            style={{ width: 300, marginBottom: "50px" }}
+            style={{ width: 300 }}
             renderInput={params => (
               <TextField
                 {...params}
@@ -83,10 +97,9 @@ class DetailsForm extends Component {
               />
             )}
             onChange={(e, value) => {
-              this.setState({ displayMpgwSelection: "block" });
+              this.setState({ mpgwList: []});
               this.setState({ displayButtons: "inline-block" });
               this.props.updateParams(value, "clusterName", "details");
-
               this.validator.message("cluster", value, "required");
               this.checkIfAllValid();
             }}
@@ -110,8 +123,7 @@ class DetailsForm extends Component {
             options={this.state.mpgwList}
             getOptionLabel={mpgwName => mpgwName}
             style={{
-              width: 300,
-              display: `${this.state.displayMpgwSelection}`
+              width: 300
             }}
             renderInput={params => (
               <TextField
@@ -123,11 +135,13 @@ class DetailsForm extends Component {
                   this.props.details.mpgwName != null
                 }
                 helperText={this.validator.getErrorMessages()["mpgw"]}
+                onChange={(e, value) => {    
+                  this.setMpgwArray(this.props.details.clusterName, this.props.details.testOrProd);
+                }}
               />
             )}
             onChange={(e, value) => {
               this.props.updateParams(value, "mpgwName", "details");
-
               this.validator.message("mpgw", value, "required");
               this.checkIfAllValid();
             }}

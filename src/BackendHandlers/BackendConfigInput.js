58abd102f656f71c464835b6a551833efdc66568
nadavatar <nadavatar1@walla.com>
2020-04-05 15:38:07 +0300

    added update mpgw backend request

--- src/BackendHandlers/BackendConfigInput.js
+++ src/BackendHandlers/BackendConfigInput.js
@@ -1,158 +1,158 @@
 export default class BackendConfigInput {
+  /**
+   * @param input -- DP MPGW information in user input style.
+   * @returns JSON Configuration (body) for backend request to create a DP HTTP FSH.
+   */
+  static generateMqFshReq(input) {
+    const config_file = require("./Configuraions/newMqFsh");
+    let config = JSON.parse(JSON.stringify(config_file));
 
-    /**
-     * @param input -- DP MPGW information in user input style.
-     * @returns JSON Configuration (body) for backend request to create a DP HTTP FSH.
-     */
-    static generateMqFshReq(input) {
+    // Acquire input from user
+    const ruleName = input["name"];
+    const queueMgr = input["srcAddr"]["primaryAddress"];
+    const queueName = input["srcAddr"]["secondaryAddress"];
 
-        const config_file = require('./Configuraions/newMqFsh');
-        let config = JSON.parse(JSON.stringify(config_file));
+    // Set configuration params
+    config["name"] = ruleName + "_FSH"; // Set FSH name
+    config["QueueManager"]["value"] = queueMgr;
+    config["GetQueue"] = queueName;
 
-        // Acquire input from user
-        const ruleName = input["name"];
-        const queueMgr = input["srcAddr"]["primaryAddress"];
-        const queueName = input["srcAddr"]["secondaryAddress"];
+    return config;
+  }
 
-        // Set configuration params
-        config["name"] = ruleName + "_FSH"; // Set FSH name
-        config["QueueManager"]["value"] = queueMgr;
-        config["GetQueue"] = queueName;
+  /**
+   * @param input -- DP rule information in user input style.
+   * @returns JSON Configuration (body) for backend request to create a DP HTTP FSH.
+   */
+  static generateHttpFshReq(input) {
+    const config_file = require("./Configuraions/newHttpFsh");
+    let config = JSON.parse(JSON.stringify(config_file));
 
-        return config;
+    // Acquire input from user
+    const ruleName = input["name"];
+    const httpMethods = input["srcAddr"]["methods"];
+    const port = input["srcAddr"]["secondaryAddress"]; // DP port to install FSH on.
+
+    // Set configuration params
+    config["name"] = ruleName + "_FSH"; // Set FSH name
+    config["LocalAddress"] = "0.0.0.0";
+    config["LocalPort"] = port;
+    // Set http methods
+    for (let i = 0; i < httpMethods.length; i++) {
+      config["AllowedFeatures"][httpMethods[i]] = "on"; // Turn on given methods.
     }
 
-    /**
-     * @param input -- DP rule information in user input style.
-     * @returns JSON Configuration (body) for backend request to create a DP HTTP FSH.
-     */
-    static generateHttpFshReq(input) {
+    return config;
+  }
 
-        const config_file = require('./Configuraions/newHttpFsh');
-        let config = JSON.parse(JSON.stringify(config_file));
+  /**
+   * @param input -- DP rule in user input style.
+   * @param num -- rule number on MPGW.
+   * @returns JSON Configuration (body) for backend request to create a DP rule.
+   */
+  static generateRuleReq(input, num) {
+    const config_file = require("./Configuraions/newRule");
+    // let config = JSON.parse(JSON.stringify(config_file))["rules"][0]; // Configuration JSON for one rule.
+    let config = JSON.parse(JSON.stringify(config_file)); // Configuration JSON for one rule.
 
-        // Acquire input from user
-        const ruleName = input["name"];
-        const httpMethods = input["srcAddr"]["methods"];
-        const port = input["srcAddr"]["secondaryAddress"]; // DP port to install FSH on.
+    // Acquire input from user
 
-        // Set configuration params
-        config["name"] = ruleName + "_FSH"; // Set FSH name
-        config["LocalAddress"] = "0.0.0.0";
-        config["LocalPort"] = port;
-        // Set http methods
-        for (let i = 0; i < httpMethods.length; i++) {
-            config["AllowedFeatures"][httpMethods[i]] = "on"; // Turn on given methods.
-        }
+    const ruleName = input["name"];
+    const fshName = ruleName + "_FSH";
+    const srcProto = input["srcAddr"]["protocol"];
+    const dstProto = input["destAddr"]["protocol"];
+    // Get input source addresses
+    const srcPAddr = input["srcAddr"]["primaryAddress"];
+    const scrSAddr = input["srcAddr"]["secondaryAddress"]; // if HTTP -- port to intsall rule on (for match action).
+    // Get input destination addresses
+    const dstPAddr = input["destAddr"]["primaryAddress"];
+    const dstSAddr = input["destAddr"]["secondaryAddress"];
 
-        return config;
+    // Set configuration
 
+    config["name"] = ruleName + "_rule"; // Set rule name
+    // Config source URL (for match rule)
+    if (srcProto === "http") {
+      config["match"]["MatchRules"]["Url"] =
+        "http://0.0.0.0:" + scrSAddr + "/*";
+    }
+    if (srcProto === "mq") {
+      config["match"]["MatchRules"]["Url"] =
+        "dpmq://" + srcPAddr + "/" + fshName + "/?RequestQueue=" + scrSAddr;
+    }
+    // Config destination URL
+    if (dstProto === "http") {
+      config["actions"][2]["StylesheetParameters"][0]["ParameterValue"] =
+        "http://" + dstPAddr + ":" + dstSAddr;
+    }
+    if (dstProto === "mq") {
+      config["actions"][2]["StylesheetParameters"][0]["ParameterValue"] =
+        "dpmq://" + dstPAddr + "/?RequestQueue=" + dstSAddr;
     }
 
-    /**
-     * @param input -- DP rule in user input style.
-     * @param num -- rule number on MPGW.
-     * @returns JSON Configuration (body) for backend request to create a DP rule.
-     */
-    static generateRuleReq(input, num) {
+    return config;
+  }
 
-        const config_file = require('./Configuraions/newRule');
-        // let config = JSON.parse(JSON.stringify(config_file))["rules"][0]; // Configuration JSON for one rule.
-        let config = JSON.parse(JSON.stringify(config_file)); // Configuration JSON for one rule.
+  /**
+   * @param input -- DP MPGW information in user input style.
+   * @returns JSON Configuration (body) for backend request to create a MPGW.
+   */
+  static generateMpgwReq(input, whichReq) {
+    // Get basic backend configuration
+    const config_file = require("./Configuraions/newMpgw");
+    let config = JSON.parse(JSON.stringify(config_file));
 
-        // Acquire input from user
+    let mpgwName = "";
+    // Acquire input from user based on the type of request to the backend
+    if (whichReq === "new") {
+      mpgwName = input["details"]["projectNameValue"];
+    } else if (whichReq === "update") {
+      mpgwName = input["details"]["mpgwName"];
+    } else {
+      console.error("Error! This request type is not supported...");
+    }
+    console.log("The mpgw name is: " + mpgwName);
 
-        const ruleName = input["name"];
-        const fshName = ruleName + "_FSH";
-        const srcProto = input["srcAddr"]["protocol"];
-        const dstProto = input["destAddr"]["protocol"];
-        // Get input source addresses
-        const srcPAddr = input["srcAddr"]["primaryAddress"];
-        const scrSAddr = input["srcAddr"]["secondaryAddress"]; // if HTTP -- port to intsall rule on (for match action).
-        // Get input destination addresses
-        const dstPAddr = input["destAddr"]["primaryAddress"];
-        const dstSAddr = input["destAddr"]["secondaryAddress"];
+    config["name"] = mpgwName; // Set MPGW name
 
-        // Set configuration
+    // Set rules
+    const rules = input["rules"];
+    let configRules = [];
+    for (let i = 0; i < rules.length; i++) {
+      let rule = this.generateRuleReq(rules[i], i);
+      configRules.push(JSON.parse(JSON.stringify(rule))); // push copied rule
+    }
+    config["rules"] = configRules;
 
-        config["name"] = ruleName + "_rule"; // Set rule name
-        // Config source URL (for match rule)
-        if (srcProto === "http") {
-            config["match"]["MatchRules"]["Url"] =
-                "http://0.0.0.0:" + scrSAddr + "/*";
-        }
-        if (srcProto === "mq") {
-            config["match"]["MatchRules"]["Url"] =
-                "dpmq://" + srcPAddr + "/" + fshName + "/?RequestQueue=" + scrSAddr;
-        }
-        // Config destination URL
-        if (dstProto === "http") {
-            config["actions"][2]["StylesheetParameters"][0]["ParameterValue"] =
-                "http://" + dstPAddr + ":" + dstSAddr;
-        }
-        if (dstProto === "mq") {
-            config["actions"][2]["StylesheetParameters"][0]["ParameterValue"] =
-                "dpmq://" + dstPAddr + "/?RequestQueue=" + dstSAddr;
-        }
-
-        return config;
+    // Add FSHs to MPGW's handlers if its a newMpgw request
+    if (whichReq === "new") {
+      let handlers = [];
+      for (let i = 0; i < rules.length; i++) {
+        let fshName = rules[i]["name"] + "_FSH";
+        handlers.push(JSON.parse(JSON.stringify(fshName)));
+      }
+      config["handlers"] = handlers;
     }
 
-    /**
-     * @param input -- DP MPGW information in user input style.
-     * @returns JSON Configuration (body) for backend request to create a MPGW.
-     */
-    static generateMpgwReq(input) {
+    console.log(config);
+    return config;
+  }
 
-        // Get basic backend configuration
-        const config_file = require('./Configuraions/newMpgw');
-        let config = JSON.parse(JSON.stringify(config_file));
-
-        // Acquire input from user
-        const mpgwName = input["details"]["projectNameValue"];
-
-        config["name"] = mpgwName; // Set MPGW name
-
-        // Set rules
-        const rules = input["rules"];
-        let configRules = [];
-        for (let i = 0; i < rules.length; i++) {
-            let rule = this.generateRuleReq(rules[i], i);
-            configRules.push(JSON.parse(JSON.stringify(rule))); // push copied rule
-        }
-        config["rules"] = configRules;
-
-        // Add FSHs to MPGW's handlers
-        let handlers = [];
-        for (let i = 0; i < rules.length; i++) {
-            let fshName = rules[i]["name"] + "_FSH";
-            handlers.push(JSON.parse(JSON.stringify(fshName)));
-
-        }
-        config["handlers"] = handlers;
-
-        console.log(config);
-        return config;
-
+  /**
+   *  Get list of url params to use for backend requests for DP cluster
+   * @param input -- DP MPGW information in user input style.
+   * @param clusterDetails -- cluster details as returned from backend.
+   */
+  static generateClusterUrlParams(input, clusterDetails) {
+    let urlParamsList = [];
+    const dpCredentials = input["dpCredentials"];
+    const nodes = clusterDetails["nodes"];
+    for (let i = 0; i < nodes.length; i++) {
+      // let urlParams = "?host=" + nodes[i]["host"] + "&port=" + nodes[i]["port"] + "&username=" +
+      //     dpCredentials["username"] + "&password=" + dpCredentials["password"] + "&domain=" + clusterDetails["domain"];
+      let urlParams = `?host=${nodes[i]["host"]}&port=${nodes[i]["port"]}&username=${dpCredentials["username"]}&password=${dpCredentials["password"]}&domain=${clusterDetails["domain"]}`;
+      urlParamsList.push(urlParams);
     }
-
-    /**
-     *  Get list of url params to use for backend requests for DP cluster
-     * @param input -- DP MPGW information in user input style.
-     * @param clusterDetails -- cluster details as returned from backend.
-     */
-    static generateClusterUrlParams(input, clusterDetails) {
-
-        let urlParamsList = [];
-        const dpCredentials = input["dpCredentials"];
-        const nodes = clusterDetails["nodes"];
-        for (let i = 0; i < nodes.length; i++) {
-            // let urlParams = "?host=" + nodes[i]["host"] + "&port=" + nodes[i]["port"] + "&username=" +
-            //     dpCredentials["username"] + "&password=" + dpCredentials["password"] + "&domain=" + clusterDetails["domain"];
-            let urlParams = `?host=${nodes[i]["host"]}&port=${nodes[i]["port"]}&username=${dpCredentials["username"]}&password=${dpCredentials["password"]}&domain=${clusterDetails["domain"]}`;
-            urlParamsList.push(urlParams);
-        }
-        return urlParamsList;
-    }
-
+    return urlParamsList;
+  }
 }
--- src/BackendHandlers/BackendRequests.js
+++ src/BackendHandlers/BackendRequests.js
@@ -1,172 +1,205 @@
-import BackendConfigInput from './BackendConfigInput.js'
+import BackendConfigInput from "./BackendConfigInput.js";
 
 export default class BackendRequests {
+  // CONSTANTS
+  // static BACKEND_URL = "http://10.0.3.8:4000";
+  static BACKEND_URL = "http://localhost:4000";
 
-    // CONSTANTS
-    // static BACKEND_URL = "http://10.0.3.8:4000";
-    static BACKEND_URL = "http://localhost:4000";
+  /**
+   * Get list of clusters for input choosing
+   * @returns {Promise<void>} [] list of cluster names from backend.
+   */
+  static async getClustersNames() {
+    const url = this.BACKEND_URL + "/api/status/cluster/prod";
+    const response = await fetch(url);
+    return await response.json(); // return clusters names.
+  }
 
-    /**
-     * Get list of clusters for input choosing
-     * @returns {Promise<void>} [] list of cluster names from backend.
-     */
-    static async getClustersNames() {
+  /**
+   * Get details (domain / IPs / rest ports) for the given cluster from input.
+   * @param input -- User input params for MPGW creation.
+   * @returns {Promise<any>}
+   */
+  static async getClusterDetails(input) {
+    const clusterName = input["details"]["clusterName"];
+    const testOrProd = input["details"]["testOrProd"];
+    const url =
+      this.BACKEND_URL +
+      "/api/status/cluster/" +
+      clusterName +
+      "/" +
+      testOrProd;
+    const response = await fetch(url);
+    return await response.json(); // return cluster details
+  }
 
-        const url = this.BACKEND_URL + "/api/status/cluster/prod";
-        const response = await fetch(url);
-        return await response.json();  // return clusters names.
+  /**
+   * Get a number of an available port on DP using http request to backend.
+   * @returns {Promise<number|*>} the number of the free port to use.
+   */
+  static async getFreePort() {
+    // const url = 'http://localhost:4000/port'; // DEBUG
+    const url = this.BACKEND_URL + "/api/status/port" + this.DP_URL_PARAMS;
+    const response = await fetch(url);
+    const portResp = await response.json();
+    if (portResp["status"] !== 200) {
+      console.log("Error finding available port");
+      return -1;
+    }
+    return portResp["message"];
+  }
+
+  /**
+   * Create New FSH on DP, using http requests to backend
+   * @param input -- User input params for rule creation.
+   * @param DpUrlParams -- params for backend url for Dp machine to create FSH on.
+   * @returns {Promise<void>}
+   */
+  static async createNewFsh(input, DpUrlParams) {
+    let url = "";
+
+    const protocol = input["srcAddr"]["protocol"];
+    let payload = {};
+    if (protocol === "") {
+      alert("Select Protocol");
+      return;
+    }
+    if (protocol === "http") {
+      payload = BackendConfigInput.generateHttpFshReq(input);
+      url = this.BACKEND_URL + "/api/http_handler" + DpUrlParams;
+    }
+    if (protocol === "mq") {
+      payload = BackendConfigInput.generateMqFshReq(input);
+      url = this.BACKEND_URL + "/api/mq_handler" + DpUrlParams;
     }
 
-    /**
-     * Get details (domain / IPs / rest ports) for the given cluster from input.
-     * @param input -- User input params for MPGW creation.
-     * @returns {Promise<any>}
-     */
-    static async getClusterDetails(input) {
-        const clusterName = input["details"]["clusterName"];
-        const testOrProd = input["details"]["testOrProd"];
-        const url = this.BACKEND_URL + "/api/status/cluster/" + clusterName + "/" + testOrProd;
-        const response = await fetch(url);
-        return await response.json();  // return cluster details
+    // url = "http://localhost:4000/"; // DEBUG
+    const data = JSON.stringify(payload);
+    const options = {
+      method: "POST",
+      body: data,
+    };
+    const response = await fetch(url, options);
+    const responseData = await response.json();
+    console.log(responseData);
+  }
+
+  /**
+   * Create FSHs For each rule created by user.
+   * @param rules -- list of rules in user input format.
+   * @param urlParamsList -- list of URL representing DP machines for backend.
+   * @returns {Promise<void>}
+   */
+  static async createNewFshs(rules, urlParamsList) {
+    for (let i = 0; i < rules.length; i++) {
+      // For each rule
+      for (let j = 0; j < urlParamsList.length; j++) {
+        // For each machine in cluster
+        this.createNewFsh(rules[i], urlParamsList[j]);
+      }
     }
+  }
 
-    /**
-     * Get a number of an available port on DP using http request to backend.
-     * @returns {Promise<number|*>} the number of the free port to use.
-     */
-    static async getFreePort() {
+  /**
+   * Create New MPGW on DP, using http requests to backend
+   * @param input -- User input params for MPGW creation.
+   * @returns {Promise<void>}
+   */
+  static async createNewMpgw(input) {
+    const clusterDetails = await this.getClusterDetails(input);
+    const urlParamsList = BackendConfigInput.generateClusterUrlParams(
+      input,
+      clusterDetails
+    );
+    const rules = input["rules"];
 
-        // const url = 'http://localhost:4000/port'; // DEBUG
-        const url = this.BACKEND_URL + "/api/status/port" + this.DP_URL_PARAMS;
-        const response = await fetch(url);
-        const portResp = await response.json();
-        if (portResp["status"] !== 200) {
-            console.log("Error finding available port");
-            return -1;
-        }
-        return portResp["message"]
+    // Create FSHs
+    this.createNewFshs(rules, urlParamsList);
 
+    // Upload files Test
+    this.uploadFiles(rules);
+
+    const payload = BackendConfigInput.generateMpgwReq(input, "new"); // Create backend configuration form input.
+    const data = JSON.stringify(payload);
+    const options = {
+      method: "POST",
+      body: data,
+    };
+
+    for (let i = 0; i < urlParamsList.length; i++) {
+      let url = this.BACKEND_URL + "/api/mpgw" + urlParamsList[i];
+      let response = await fetch(url, options);
+      let responseData = await response.json();
+      console.log(responseData);
+      alert(responseData["message"]);
     }
+  }
 
-    /**
-     * Create New FSH on DP, using http requests to backend
-     * @param input -- User input params for rule creation.
-     * @param DpUrlParams -- params for backend url for Dp machine to create FSH on.
-     * @returns {Promise<void>}
-     */
-    static async createNewFsh(input, DpUrlParams) {
+  /**
+   * Create New MPGW on DP, using http requests to backend
+   * @param input -- User input params for updating an existing MPGW.
+   * @returns {Promise<void>}
+   */
+  static async updateMpgw(input) {
+    const clusterDetails = await this.getClusterDetails(input);
+    const urlParamsList = BackendConfigInput.generateClusterUrlParams(
+      input,
+      clusterDetails
+    );
+    const rules = input["rules"];
 
-        let url = "";
+    //Not Sure if we need this here, up to you Bareli
+    // Create FSHs
+    // this.createNewFshs(rules, urlParamsList);
 
-        const protocol = input["srcAddr"]["protocol"];
-        let payload = {};
-        if (protocol === "") {
-            alert("Select Protocol");
-            return;
-        }
-        if (protocol === "http") {
-            payload = BackendConfigInput.generateHttpFshReq(input);
-            url = this.BACKEND_URL + "/api/http_handler" + DpUrlParams;
-        }
-        if (protocol === "mq") {
-            payload = BackendConfigInput.generateMqFshReq(input);
-            url = this.BACKEND_URL + "/api/mq_handler" + DpUrlParams;
-        }
+    // Upload files Test
+    this.uploadFiles(rules);
 
-        // url = "http://localhost:4000/"; // DEBUG
-        const data = JSON.stringify(payload);
-        const options = {
-            method: 'POST',
-            body: data,
-        };
-        const response = await fetch(url, options);
-        const responseData = await response.json();
-        console.log(responseData);
+    const payload = BackendConfigInput.generateMpgwReq(input, "update"); // Create backend configuration form input.
+    const data = JSON.stringify(payload);
+    const options = {
+      method: "PUT",
+      body: data,
+    };
 
+    for (let i = 0; i < urlParamsList.length; i++) {
+      let url = this.BACKEND_URL + "/api/mpgw" + urlParamsList[i];
+      let response = await fetch(url, options);
+      let responseData = await response.json();
+      console.log(responseData);
+      alert(responseData["message"]);
     }
+  }
 
-    /**
-     * Create FSHs For each rule created by user.
-     * @param rules -- list of rules in user input format.
-     * @param urlParamsList -- list of URL representing DP machines for backend.
-     * @returns {Promise<void>}
-     */
-    static async createNewFshs(rules, urlParamsList) {
-        for (let i = 0; i < rules.length; i++) {  // For each rule
-            for (let j = 0; j < urlParamsList.length; j++) {  // For each machine in cluster
-                this.createNewFsh(rules[i], urlParamsList[j]);
-            }
-        }
+  /**
+   * Upload files required for MPGW creation.
+   * @param rules -- Rules in user input format.
+   */
+  static uploadFiles(rules) {
+    for (let i = 0; i < rules.length; i++) {
+      if (rules[i]["filter"]["filterType"] === "schema") {
+        this.uploadFile(rules[i]["filter"]["schemaPath"]);
+      }
     }
+  }
 
+  /**
+   * Upload file to DP using backend request to git.
+   * @param file -- JSON representing schema file from user input
+   * @returns {Promise<void>}
+   */
+  static async uploadFile(file) {
+    const url = this.BACKEND_URL + "/api/misc/uploadfile";
+    const options = {
+      method: "POST",
+      body: file["content"],
+      headers: {
+        filename: file["name"],
+      },
+    };
+    let response = await fetch(url, options);
+    let responseData = await response.json();
 
-    /**
-     * Create New MPGW on DP, using http requests to backend
-     * @param input -- User input params for MPGW creation.
-     * @returns {Promise<void>}
-     */
-    static async createNewMpgw(input) {
-
-        const clusterDetails = await this.getClusterDetails(input);
-        const urlParamsList = BackendConfigInput.generateClusterUrlParams(input, clusterDetails);
-        const rules = input["rules"];
-
-        // Create FSHs
-        this.createNewFshs(rules, urlParamsList);
-
-        // Upload files Test
-        this.uploadFiles(rules);
-
-        const payload = BackendConfigInput.generateMpgwReq(input); // Create backend configuration form input.
-        const data = JSON.stringify(payload);
-        const options = {
-            method: 'POST',
-            body: data,
-        };
-
-        for (let i = 0; i < urlParamsList.length; i++) {
-            let url = this.BACKEND_URL + "/api/mpgw" + urlParamsList[i];
-            let response = await fetch(url, options);
-            let responseData = await response.json();
-            console.log(responseData);
-            alert(responseData["message"]);
-        }
-
-    }
-
-
-    /**
-     * Upload files required for MPGW creation.
-     * @param rules -- Rules in user input format.
-     */
-    static uploadFiles(rules) {
-        for (let i = 0; i < rules.length; i++) {
-            if (rules[i]["filter"]["filterType"] === "schema") {
-                this.uploadFile(rules[i]["filter"]["schemaPath"]);
-            }
-        }
-    }
-
-    /**
-     * Upload file to DP using backend request to git.
-     * @param file -- JSON representing schema file from user input
-     * @returns {Promise<void>}
-     */
-    static async uploadFile(file) {
-
-        const url = this.BACKEND_URL + "/api/misc/uploadfile";
-        const options = {
-            method: 'POST',
-            body: file["content"],
-            headers: {
-                filename: file["name"]
-            }
-        };
-        let response = await fetch(url, options);
-        let responseData = await response.json();
-
-        alert(responseData);
-    }
-
-
+    alert(responseData);
+  }
 }
